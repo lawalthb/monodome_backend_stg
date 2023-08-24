505c20f2f4d75fda4cce29d06ac0682cb04453a2
DON EJEH1~ <donejeh@gmail.com>
2023-08-24 22:00:40 +0100

    organizing routes

---
+++ routes/api/v1/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::group(['namespace' => 'api\v1', 'prefix' => 'v1'], function () {
+
+    Route::group(['prefix' => 'auth', 'namespace' => 'auth'], function () {
+
+
+});
+
+
+});
+
+?>
