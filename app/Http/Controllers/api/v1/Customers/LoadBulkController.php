f5f79b2f44b90aedddadbbab48c8436e617684cf
DON EJEH1~ <donejeh@gmail.com>
2023-09-01 19:07:39 +0100

    added controller for all the loads

---
+++ app/Http/Controllers/api/v1/Customers/LoadBulkController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\api\v1\Customers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoadBulkController extends Controller
+{
+    //
+}
