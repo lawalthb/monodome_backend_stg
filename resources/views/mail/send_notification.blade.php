f9a1b45288b9f0b4c56ed9c931a8d72a6f007b2f
DON EJEH1~ <donejeh@gmail.com>
2023-09-15 14:04:35 +0100

    location

--- resources/views/mail/send_notification.blade.php
+++ resources/views/mail/send_notification.blade.php
@@ -4,9 +4,16 @@
 {{  $message }}
 
 <x-mail::button :url="$url">
-Check it
+You can continue from here
 </x-mail::button>
 
+@if($user['location'] != null){
+<x-mail::table>
+| Ip       | Country         | State  |
+| ------------- |:-------------:| --------:|
+|    {{  $user['location']['ip'] }}    | {{ $user['location']['countryName'] }}      | {{ $user['location']['regionName'] }}      |
+</x-mail::table>
+}
 Thanks,<br>
 {{ config('app.name') }}
 </x-mail::message>
