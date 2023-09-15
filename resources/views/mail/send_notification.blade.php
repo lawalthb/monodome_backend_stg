863e61401959d39ed52eb0d16c4f4cf2bf3df70e
DON EJEH1~ <donejeh@gmail.com>
2023-09-15 14:43:08 +0100

    <em>

--- resources/views/mail/send_notification.blade.php
+++ resources/views/mail/send_notification.blade.php
@@ -8,14 +8,14 @@ You can continue from here
 </x-mail::button> --}}
 
 @if($user['location'] != null)
-
-{{  $user['location']['ip'] }}    | {{ $user['location']['countryName'] }}      | {{ $user['location']['regionName'] }}      |
-
-
+<em>
+Location: {{  $user['location']['ip'] }}  | {{ $user['location']['countryName'] }} | {{ $user['location']['regionName'] }}
+</em>
 @endif
 
-<p style="color: red">Remember:</p> Keep your card and Pin information secure.
+<em style="color: red">Remember:</em> <em>Keep your card and Pin information secure.
 Do not respond to emails requesting for your card/PIN details. If you think an email is suspicious, don't click on any links. Instead, forward it to our customer support.
+</em>
 <br>
 Thanks,<br> <br>
 {{ config('app.name') }}
