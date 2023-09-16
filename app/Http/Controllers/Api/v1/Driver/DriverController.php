ee9a59770027707dafe0c9c3b8b7f91d2fcc7644
DON EJEH1~ <donejeh@gmail.com>
2023-09-16 18:25:52 +0100

    DriverRequest

--- app/Http/Controllers/Api/v1/Driver/DriverController.php
+++ app/Http/Controllers/Api/v1/Driver/DriverController.php
@@ -90,7 +90,7 @@ class DriverController extends Controller
         return LoadBoardResource::collection($loadBoards);
     }
 
-    public function store(Request $request)
+    public function store(DriverRequest $request)
     {
         try {
             DB::beginTransaction();
