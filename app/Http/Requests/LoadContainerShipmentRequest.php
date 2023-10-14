dc55d000a203de98a0eb370171b8a00d7a993e0d
DON EJEH1~ <donejeh@gmail.com>
2023-10-14 19:57:59 +0100

    required_if

--- app/Http/Requests/LoadContainerShipmentRequest.php
+++ app/Http/Requests/LoadContainerShipmentRequest.php
@@ -39,7 +39,7 @@ class LoadContainerShipmentRequest extends FormRequest
             'receiver_email' => 'required_if:is_final,Yes|email',
             'receiver_phone' => 'required_if:is_final,Yes|string|max:30',
           //  'zip_code' => 'required|string|max:30',
-            'street' => 'required|string|max:30',
+            'street' => 'required_if:is_final,Yes|string|max:30',
             'add_info' => 'nullable|string',
             //'insure_it' => 'nullable|in:Yes,No',
             'suggested_amount' => 'nullable|integer',
