c5e996b4f79f800b59a6677e7571835d133853cd
DON EJEH1~ <donejeh@gmail.com>
2023-10-15 10:52:13 +0100

    Driver Manager

--- app/Models/DriverManger.php
+++ app/Models/DriverManger.php
@@ -2,10 +2,55 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class DriverManger extends Model
 {
     use HasFactory;
+
+
+    public $guarded = [];
+
+    public function guarantors()
+    {
+        return $this->morphMany(Guarantor::class, 'loadable');
+    }
+
+    public function loadable()
+    {
+        return $this->morphTo('loadable');
+    }
+
+    public function user(){
+
+        return $this->belongsTo(User::class);
+    }
+
+    public function country(){
+
+        return $this->belongsTo(Country::class);
+    }
+
+    public function state(){
+
+        return $this->belongsTo(LocalState::class);
+    }
+
+    public function local(){
+
+        return $this->belongsTo(LocalGovernment::class,'lga');
+    }
+
+
+    protected static function boot()
+    {
+        parent::boot();
+        self::creating(function($model){
+            $model->uuid =  Str::uuid()->toString();
+           // $model->user_id =  auth()->id();
+        });
+    }
+
 }
