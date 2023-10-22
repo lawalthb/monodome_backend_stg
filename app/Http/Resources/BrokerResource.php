50cb663f969e0c657273ae20edc23a85ef2115c2
DON EJEH1~ <donejeh@gmail.com>
2023-10-22 07:45:11 +0100

    LocalGovernmentResource

--- app/Http/Resources/BrokerResource.php
+++ app/Http/Resources/BrokerResource.php
@@ -20,13 +20,13 @@ class BrokerResource extends JsonResource
            'email' => $this->user->email,
           'street' => $this->street,
         //  'phone_number' => $this->phone_number,
-          "lga" => new LocalGovernmentResource($this->SLga),
-          'profile_picture' =>  getImageFile($this->profile_picture),
-          'status' => $this->status,
-          'created_at' => $this->created_at,
-          'updated_at' => $this->updated_at,
-          'user' => new UserResource($this->user),
-          'state' => new StateResource($this->state),
+        'profile_picture' =>  getImageFile($this->profile_picture),
+        'status' => $this->status,
+        'created_at' => $this->created_at,
+        'updated_at' => $this->updated_at,
+        'user' => new UserResource($this->user),
+        'state' => new StateResource($this->state),
+        "lga" => new LocalGovernmentResource($this->SLga),
         ];
     }
 }
