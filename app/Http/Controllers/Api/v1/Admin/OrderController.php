9c7d8135921cd544e8696497c03aaa71b8154ffb
DON EJEH1~ <donejeh@gmail.com>
2023-11-16 22:11:31 +0100

    OrderController

---
+++ app/Http/Controllers/Api/v1/Admin/OrderController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Admin;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\OrderResource;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $key = $request->input('search');
+        $perPage = $request->input('per_page', 10);
+
+        $agents = Order::where(function ($q) use ($key) {
+            // Assuming there's a relationship between Agent and User
+            $q->whereHas('user', function ($userQuery) use ($key) {
+                $userQuery->where('full_name', 'like', "%{$key}%");
+            })->orWhere('order_no', 'like', "%{$key}%");
+        })
+            ->latest()
+            ->paginate($perPage);
+
+        return OrderResource::collection($agents);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $order = Order::find($id);
+
+        if (!$order) {
+
+            return $this->error('', 'order not found', 422);
+
+        }
+
+        return new OrderResource($order);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
