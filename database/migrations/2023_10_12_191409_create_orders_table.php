c581e69b7aa3cba94f877476e0a218f98427cd61
DON EJEH1~ <donejeh@gmail.com>
2023-12-04 21:43:50 +0100

    acceptable_id

--- database/migrations/2023_10_12_191409_create_orders_table.php
+++ database/migrations/2023_10_12_191409_create_orders_table.php
@@ -22,6 +22,8 @@ return new class extends Migration
             $table->enum('accepted', ['Yes', 'No', 'Rejected'])->default('No');
             $table->decimal('amount',38, 18)->default(0);
             $table->decimal('fee',38, 18)->default(0);
+            $table->unsignedBigInteger('acceptable_id')->nullable();
+            $table->string('acceptable_type')->nullable();
             $table->unsignedBigInteger('loadable_id'); // Foreign key to the associated load
             $table->string('loadable_type'); // Type of load (e.g., 'load_packages', 'load_specialized', etc.)
             $table->enum('status', ['Pending', 'Confirmed', 'Failed', 'Paid','Approved','Processing'])->default('Pending');
