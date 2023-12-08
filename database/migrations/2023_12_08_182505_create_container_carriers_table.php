de80e2ab03d5bd99adf6d00a3ed455e6fa9a1bdc
DON EJEH1~ <donejeh@gmail.com>
2023-12-08 19:25:48 +0100

    container_carriers

---
+++ database/migrations/2023_12_08_182505_create_container_carriers_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('container_carriers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+
+
+        // Insert carriers data into the table
+        $carriers = [
+            'Mediterranean Shipping Company',
+            'AP Moller-Maersk Group',
+            'CMA CGM Group',
+            'China Cosco Group',
+            'Hapag-Lloyd',
+            'Evergreen Marine Corporation',
+            'Ocean Network Express',
+            'Hyundai Merchant Marine',
+            'Yang Ming Marine Transport Corporation',
+            'Zim Integrated Shipping Services',
+        ];
+
+        foreach ($carriers as $carrier) {
+            DB::table('container_carriers')->insert([
+                'name' => $carrier,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('container_carriers');
+    }
+};
