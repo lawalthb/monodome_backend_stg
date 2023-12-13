46d001cbd7aca4ae5341bfe6364228239f32c40c
DON EJEH1~ <donejeh@gmail.com>
2023-12-13 04:28:27 +0100

    lockForUpdate

--- app/Http/Controllers/Api/v1/Order/OrderController.php
+++ app/Http/Controllers/Api/v1/Order/OrderController.php
@@ -10,6 +10,7 @@ use App\Models\WalletHistory;
 use App\Traits\ApiStatusTrait;
 use App\Events\LoadTypeCreated;
 use App\Models\DistanceSetting;
+use Illuminate\Support\Facades\DB;
 use App\Http\Requests\OrderRequest;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
@@ -44,106 +45,197 @@ class OrderController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(OrderRequest $request)
-    {
-        $loadType = LoadType::find($request->load_type_id);
-
-        if (!$loadType) {
-
-            return $this->error('', 'LoadType not found', 404);
-        }
-
-        $specificType = $loadType->specificType;
-        if (!$specificType) {
-            return $this->error('', 'Specific type not found', 404);
-        }
-
-        //this get load to pay
-        $load = $specificType->where('id', $request->load_id)->first();
-
-        if (!$load) {
-            return $this->error('', 'Load not found', 404);
-        }
-
-        if (!$load->user->wallet) {
-            return $this->error('', 'wallet not setup', 404);
-        }
-
-        if ($load->total_amount == 0) {
-            return $this->error('', 'order amount cant be zero', 404);
-        }
-
-       // Log::info($loadType->loadable_type);
-
-        $loadTotalAmount = number_format($load->total_amount, 2, '.', ''); // Format as a string with 2 decimal places
-        $userWalletAmount = number_format($load->user->wallet->amount, 2, '.', '');
-
-        if ($loadTotalAmount >= $userWalletAmount) {
-            return $this->error('', 'Insufficient funds in wallet!', 404);
-        }
-
-       // $order = Order::where(['user_id'=>Auth::id(),'loadable_id'=>$load->id,'status'=>'Paid'])->first();
-       $order = Order::where([
-           'user_id' => Auth::id(),
-           'loadable_id' => $load->id,
-           'loadable_type' => get_class($load), // Add loadable_type condition
-           'status' => 'Paid',
-           ])->first();
-
-         //  Log::info($order);
-        if($order){
-            return $this->error('', 'This Order has already been paid!', 404);
-
-        }
-        $load->user->wallet->amount =  $load->user->wallet->amount - $loadTotalAmount;
-
-       // ,'Approved','Processing'
-        $load->status = 'Waiting';
-        $load->save();
-        $order = Order::updateOrCreate(
-            [
-                'user_id' => $load->user_id,
-                'loadable_id' => $load->id, // Add loadable_id condition
-                'loadable_type' => get_class($load),
-            ],
-            [
-             //   'order_no' => getNumber(),
-                'driver_id' => 1,
-                'amount' => $loadTotalAmount,
-                'status' => "Paid",
-            ]
-        );
-
-        // Associate the order with the load
-        $order->loadable()->associate($load);
-        //  $order->save();
-
-        if ($order->save()) {
-
-               // Create a wallet history entry
-               $walletHistory = new WalletHistory;
-               $walletHistory->wallet_id = $load->user->wallet->id;
-               $walletHistory->user_id =$load->user->id;
-               $walletHistory->type = "debit";
-               $walletHistory->payment_type = "wallet";
-               $walletHistory->amount = $load->total_amount;
-               $walletHistory->closing_balance = $load->user->wallet->amount;
-               $walletHistory->fee = 0;
-               $walletHistory->description = "Payment for Order with the follow ID: ".$order->order_no. " !";
-               $walletHistory->save();
 
 
-            $message ="Your Order with the follow ID: ".$order->order_no. " was successfully!";
-            $order->user->notify(new SendNotification($order->user, $message));
+     public function store(OrderRequest $request)
+     {
+         return DB::transaction(function () use ($request) {
+             $loadType = LoadType::find($request->load_type_id);
 
-            event(new LoadTypeCreated($load));
+             if (!$loadType) {
+                 return $this->error('', 'LoadType not found', 404);
+             }
 
-            return $this->success(new OrderResource($order), 'Order payment was successfully');
-        } else {
+             $specificType = $loadType->specificType;
+             if (!$specificType) {
+                 return $this->error('', 'Specific type not found', 404);
+             }
 
-            return $this->error([], 'Error placing other', 500);
-        }
-    }
+            //  $load = $specificType->where('id', $request->load_id)->first();
+            $load = $specificType->where('id', $request->load_id)->lockForUpdate()->first();
+
+             if (!$load) {
+                 return $this->error('', 'Load not found', 404);
+             }
+
+             if (!$load->user->wallet) {
+                 return $this->error('', 'Wallet not set up', 404);
+             }
+
+             if ($load->total_amount == 0) {
+                 return $this->error('', 'Order amount cannot be zero', 404);
+             }
+
+             $loadTotalAmount = number_format($load->total_amount, 2, '.', '');
+             $userWalletAmount = number_format($load->user->wallet->amount, 2, '.', '');
+
+             if ($loadTotalAmount >= $userWalletAmount) {
+                 return $this->error('', 'Insufficient funds in wallet!', 404);
+             }
+
+             $existingOrder = Order::where([
+                 'user_id' => Auth::id(),
+                 'loadable_id' => $load->id,
+                 'loadable_type' => get_class($load),
+                 'status' => 'Paid',
+             ])->first();
+
+             if ($existingOrder) {
+                 return $this->error('', 'This order has already been paid!', 404);
+             }
+
+             $load->user->wallet->amount -= $loadTotalAmount;
+             $load->status = 'Waiting';
+             $load->save();
+
+             $order = Order::updateOrCreate(
+                 [
+                     'user_id' => $load->user_id,
+                     'loadable_id' => $load->id,
+                     'loadable_type' => get_class($load),
+                 ],
+                 [
+                     'driver_id' => 1,
+                     'amount' => $loadTotalAmount,
+                     'status' => 'Paid',
+                 ]
+             );
+
+             $order->loadable()->associate($load);
+
+             if ($order->save()) {
+                 $walletHistory = new WalletHistory;
+                 $walletHistory->wallet_id = $load->user->wallet->id;
+                 $walletHistory->user_id = $load->user->id;
+                 $walletHistory->type = "debit";
+                 $walletHistory->payment_type = "wallet";
+                 $walletHistory->amount = $load->total_amount;
+                 $walletHistory->closing_balance = $load->user->wallet->amount;
+                 $walletHistory->fee = 0;
+                 $walletHistory->description = "Payment for Order with the follow ID: " . $order->order_no . " !";
+                 $walletHistory->save();
+
+                 $order->user->notify(new SendNotification($order->user, 'Your order was successful!'));
+                 event(new LoadTypeCreated($load));
+
+                 return $this->success(new OrderResource($order), 'Order payment was successful');
+             } else {
+                 return $this->error([], 'Error placing order', 500);
+             }
+         });
+     }
+
+
+    // public function store(OrderRequest $request)
+    // {
+    //     $loadType = LoadType::find($request->load_type_id);
+
+    //     if (!$loadType) {
+
+    //         return $this->error('', 'LoadType not found', 404);
+    //     }
+
+    //     $specificType = $loadType->specificType;
+    //     if (!$specificType) {
+    //         return $this->error('', 'Specific type not found', 404);
+    //     }
+
+    //     //this get load to pay
+    //     $load = $specificType->where('id', $request->load_id)->first();
+
+    //     if (!$load) {
+    //         return $this->error('', 'Load not found', 404);
+    //     }
+
+    //     if (!$load->user->wallet) {
+    //         return $this->error('', 'wallet not setup', 404);
+    //     }
+
+    //     if ($load->total_amount == 0) {
+    //         return $this->error('', 'order amount cant be zero', 404);
+    //     }
+
+    //    // Log::info($loadType->loadable_type);
+
+    //     $loadTotalAmount = number_format($load->total_amount, 2, '.', ''); // Format as a string with 2 decimal places
+    //     $userWalletAmount = number_format($load->user->wallet->amount, 2, '.', '');
+
+    //     if ($loadTotalAmount >= $userWalletAmount) {
+    //         return $this->error('', 'Insufficient funds in wallet!', 404);
+    //     }
+
+    //    // $order = Order::where(['user_id'=>Auth::id(),'loadable_id'=>$load->id,'status'=>'Paid'])->first();
+    //    $order = Order::where([
+    //        'user_id' => Auth::id(),
+    //        'loadable_id' => $load->id,
+    //        'loadable_type' => get_class($load), // Add loadable_type condition
+    //        'status' => 'Paid',
+    //        ])->first();
+
+    //      //  Log::info($order);
+    //     if($order){
+    //         return $this->error('', 'This Order has already been paid!', 404);
+
+    //     }
+    //     $load->user->wallet->amount =  $load->user->wallet->amount - $loadTotalAmount;
+
+    //    // ,'Approved','Processing'
+    //     $load->status = 'Waiting';
+    //     $load->save();
+    //     $order = Order::updateOrCreate(
+    //         [
+    //             'user_id' => $load->user_id,
+    //             'loadable_id' => $load->id, // Add loadable_id condition
+    //             'loadable_type' => get_class($load),
+    //         ],
+    //         [
+    //          //   'order_no' => getNumber(),
+    //             'driver_id' => 1,
+    //             'amount' => $loadTotalAmount,
+    //             'status' => "Paid",
+    //         ]
+    //     );
+
+    //     // Associate the order with the load
+    //     $order->loadable()->associate($load);
+    //     //  $order->save();
+
+    //     if ($order->save()) {
+
+    //            // Create a wallet history entry
+    //            $walletHistory = new WalletHistory;
+    //            $walletHistory->wallet_id = $load->user->wallet->id;
+    //            $walletHistory->user_id =$load->user->id;
+    //            $walletHistory->type = "debit";
+    //            $walletHistory->payment_type = "wallet";
+    //            $walletHistory->amount = $load->total_amount;
+    //            $walletHistory->closing_balance = $load->user->wallet->amount;
+    //            $walletHistory->fee = 0;
+    //            $walletHistory->description = "Payment for Order with the follow ID: ".$order->order_no. " !";
+    //            $walletHistory->save();
+
+
+    //         $message ="Your Order with the follow ID: ".$order->order_no. " was successfully!";
+    //         $order->user->notify(new SendNotification($order->user, $message));
+
+    //         event(new LoadTypeCreated($load));
+
+    //         return $this->success(new OrderResource($order), 'Order payment was successfully');
+    //     } else {
+
+    //         return $this->error([], 'Error placing other', 500);
+    //     }
+    // }
 
     /**
      * Display the specified resource.
