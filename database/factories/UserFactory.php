0ce1d640f37c7f9eb20848f51d08f8daf914a19c
DON EJEH1~ <donejeh@gmail.com>
2023-12-20 08:08:27 +0100

    UsersTableSeeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,12 +18,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-
             'full_name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'phone_number' => fake()->e164PhoneNumber(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'provider_id' => NULL,
             'provider' => NULL,
             'address' => fake()->streetAddress(),
