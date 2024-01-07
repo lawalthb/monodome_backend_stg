12b6cb9681e656e2de6085c871b4977a5b89fe5a
DON EJEH1~ <donejeh@gmail.com>
2024-01-07 10:59:55 +0100

    pendingBlog

--- app/Http/Controllers/Api/v1/Blog/BlogController.php
+++ app/Http/Controllers/Api/v1/Blog/BlogController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Api\v1\Blog;
 
 use App\Models\Blog;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 use App\Traits\ApiStatusTrait;
 use App\Traits\FileUploadTrait;
 use App\Http\Requests\BlogRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\BlogResource;
+use App\Http\Requests\CommentRequest;
+use App\Http\Resources\CommentResource;
 use App\Notifications\SendNotification;
 
 class BlogController extends Controller
@@ -22,6 +25,22 @@ class BlogController extends Controller
         $perPage = $request->input('per_page', 10);
 
         $blog = Blog::with('comments')->where(function ($q) use ($key) {
+            $q->whereHas('user', function ($userQuery) use ($key) {
+                $userQuery->where('full_name', 'like', "%{$key}%")
+                    ->orWhere('email', 'like', "%{$key}%");
+            })->orWhere('title', 'like', "%{$key}%");
+        })->latest()->paginate($perPage);
+
+        return BlogResource::collection($blog);
+    }
+
+
+    public function pendingBlog(Request $request)
+    {
+        $key = $request->input('search');
+        $perPage = $request->input('per_page', 10);
+
+        $blog = Blog::where('status',0)->with('comments')->where(function ($q) use ($key) {
             $q->whereHas('user', function ($userQuery) use ($key) {
                 $userQuery->where('full_name', 'like', "%{$key}%")
                     ->orWhere('email', 'like', "%{$key}%");
@@ -96,7 +115,6 @@ class BlogController extends Controller
         );
     }
 
-
     public function destroy($id)
     {
         $blog = Blog::findOrFail($id);
@@ -104,4 +122,35 @@ class BlogController extends Controller
 
         return response()->json(['message' => 'Blog deleted successfully']);
     }
+
+
+    public function getComments($blogId)
+    {
+        $comments = Comment::where('blog_id', $blogId)->get();
+        return CommentResource::collection($comments);
+    }
+
+    // Store a new comment
+    public function storeComment(CommentRequest $request)
+    {
+        $comment = Comment::create($request->validated());
+        return new CommentResource($comment);
+    }
+
+    // Update a comment
+    public function updateComment(CommentRequest $request, $id)
+    {
+        $comment = Comment::findOrFail($id);
+        $comment->update($request->validated());
+        return new CommentResource($comment);
+    }
+
+    // Delete a comment
+    public function destroyComment($id)
+    {
+        $comment = Comment::findOrFail($id);
+        $comment->delete();
+        return response()->json(['message' => 'Comment deleted successfully']);
+    }
+
 }
