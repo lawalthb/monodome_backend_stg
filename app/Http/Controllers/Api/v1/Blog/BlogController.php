ca02a4cb1fac929dcc79f3d0dffbf7ff79e4b392
DON EJEH1~ <donejeh@gmail.com>
2024-01-07 10:43:36 +0100

    blog

--- app/Http/Controllers/Api/v1/Blog/BlogController.php
+++ app/Http/Controllers/Api/v1/Blog/BlogController.php
@@ -13,45 +13,95 @@ use App\Notifications\SendNotification;
 
 class BlogController extends Controller
 {
-    use ApiStatusTrait,FileUploadTrait;
+    use ApiStatusTrait, FileUploadTrait;
 
 
     public function index(Request $request)
-{
-    $key = $request->input('search');
-    $perPage = $request->input('per_page', 10);
+    {
+        $key = $request->input('search');
+        $perPage = $request->input('per_page', 10);
 
-    $blog = Blog::with('comments')->where(function ($q) use ($key) {
-        $q->whereHas('user', function ($userQuery) use ($key) {
-            $userQuery->where('full_name', 'like', "%{$key}%")
-            ->orWhere('email','like', "%{$key}%");
-        })->orWhere('title', 'like', "%{$key}%");
-    })->latest()->paginate($perPage);
+        $blog = Blog::with('comments')->where(function ($q) use ($key) {
+            $q->whereHas('user', function ($userQuery) use ($key) {
+                $userQuery->where('full_name', 'like', "%{$key}%")
+                    ->orWhere('email', 'like', "%{$key}%");
+            })->orWhere('title', 'like', "%{$key}%");
+        })->latest()->paginate($perPage);
 
-    return BlogResource::collection($blog);
-}
+        return BlogResource::collection($blog);
+    }
 
-public function store(BlogRequest $request)
-{
-    $blog = Blog::create([
-        'user_id' => auth()->user()->id,
-        'title'   => $request->get('title'),
-        'body'   => $request->get('body'),
-        'image'=> $this->uploadFile('blog/', $request->file('image'))
-    ]);
+    public function show($id)
+    {
+        $blog = Blog::findOrFail($id);
 
-    $user = auth()->user();
+        return $this->success(
+            [
+                "post" => new BlogResource($blog),
+            ],
+            "Article has Successfully been posted"
+        );
+    }
 
-    $message ="Your Blog article has been posted, awaiting for admin for approval";
-    $user->notify(new SendNotification($user, $message));
 
-    return $this->success(
-        [
-            "post" => new BlogResource($blog),
-        ],
-        "Article has Successfully been posted"
-    );
+    public function store(BlogRequest $request)
+    {
+        $blog = Blog::create([
+            'user_id' => auth()->user()->id,
+            'title'   => $request->get('title'),
+            'body'   => $request->get('body'),
+            'image' => $this->uploadFile('blog/', $request->file('image'))
+        ]);
 
-}
+        $user = auth()->user();
 
+        $message = "Your Blog article has been posted, awaiting for admin for approval";
+        $user->notify(new SendNotification($user, $message));
+
+        return $this->success(
+            [
+                "post" => new BlogResource($blog),
+            ],
+            "Article has Successfully been posted"
+        );
+    }
+
+
+    public function update(BlogRequest $request, $id)
+    {
+        $blog = Blog::findOrFail($id);
+
+
+        if ($request->hasFile('image') && $blog->image !== null) {
+            $this->deleteFile($blog->image);
+        }
+
+        $blog->update([
+            'title' => $request->get('title'),
+            'body' => $request->get('body'),
+            //'image'=> $this->uploadFile('blog/', $request->file('image'))
+        ]);
+
+        if ($request->hasFile('image')) {
+            $imagePath = $this->uploadFile('blog/', $request->file('image'));
+            $blog->image = $imagePath;
+            $blog->save();
+        }
+
+        return $this->success(
+            [
+                "post" => new BlogResource($blog),
+            ],
+            "Article has been updated successfully"
+        );
+    }
+
+
+    public function destroy($id)
+    {
+        $blog = Blog::findOrFail($id);
+        $blog->delete();
+
+        return response()->json(['message' => 'Blog deleted successfully']);
+    }
 }
