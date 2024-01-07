0ba3623192ccae4eb664a4635b7ccaa4b03086bb
DON EJEH1~ <donejeh@gmail.com>
2024-01-07 09:37:04 +0100

    BlogRequest

--- app/Http/Controllers/Api/v1/Blog/BlogController.php
+++ app/Http/Controllers/Api/v1/Blog/BlogController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers\Api\v1\Blog;
 
-use App\Http\Controllers\Controller;
+use App\Models\Blog;
 use Illuminate\Http\Request;
+use App\Traits\ApiStatusTrait;
+use App\Traits\FileUploadTrait;
+use App\Http\Controllers\Controller;
 
 class BlogController extends Controller
 {
-    //
+    use ApiStatusTrait,FileUploadTrait;
+
+
+
+    public function index()
+{
+    $posts = Blog::with('comments')->get();
+    return response()->json(['posts' => $posts]);
+}
 }
