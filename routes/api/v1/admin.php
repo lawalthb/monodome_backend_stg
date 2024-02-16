ede2cb77f2c70f56fac4e94ea250ad279562b309
DON EJEH1~ <donejeh@gmail.com>
2024-02-16 21:31:37 +0100

    loadBoard-order

--- routes/api/v1/admin.php
+++ routes/api/v1/admin.php
@@ -90,6 +90,7 @@ Route::group(['namespace' => 'api\v1', 'prefix' => 'v1/admin', 'middleware' => '
         Route::get('/user/{id}', [OrderController::class, 'all_user_orders']);
         Route::post('/payment-status/status', [OrderController::class, 'paymentOrderStatus']);
         Route::post('/approve-order/status', [OrderController::class, 'approveOrderStatus']);
+        Route::post('/loadBoard-order/status', [OrderController::class, 'loadBoardOrderStatus']);
 
     });
         Route::group(['prefix' => 'transactions'], function () {
