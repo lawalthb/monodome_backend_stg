5415cb27967d29ca567b4aa885ed6b4eb77b9287
DON EJEH <donejeh@gmail.com>
2024-02-20 13:18:59 +0100

    status

--- app/Http/Controllers/Api/v1/Customers/LoadBulkController.php
+++ app/Http/Controllers/Api/v1/Customers/LoadBulkController.php
@@ -81,7 +81,7 @@ class LoadBulkController extends Controller
                 'amount' =>  $totalAmount,
                 'fee' =>  $validatedData['delivery_fee'],
                 'user_id' => $loadBulk->user_id,
-                'status' => "Pending",
+               // 'status' => "Pending",
             ]);
         } else {
             $order = $loadBulk->order;
