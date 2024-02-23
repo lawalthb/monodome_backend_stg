068fdd57d191dd31831dba7335e02869e5d5970b
DON EJEH1~ <donejeh@gmail.com>
2024-02-23 19:31:29 +0100

    employees

--- database/migrations/2023_09_19_140814_create_employees_table.php
+++ database/migrations/2023_09_19_140814_create_employees_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
             $table->string('uuid');
-            $table->unsignedBigInteger('user_id')->index('user_id');
+            $table->unsignedBigInteger('user_id')->index('user_id')->nullable();
             $table->string('full_name');
             $table->string('address');
             $table->string('phone_number');
