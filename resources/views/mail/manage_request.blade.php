3a78cbdc8e28abb9a8ae44cbfa8c97816393d7bf
DON EJEH1~ <donejeh@gmail.com>
2024-02-27 18:35:08 +0100

    rawurlencode

--- resources/views/mail/manage_request.blade.php
+++ resources/views/mail/manage_request.blade.php
@@ -3,25 +3,28 @@
 <head>
     <title>Email Template</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Hi {{ $user['full_name'] }}</h1>
+    <div class="container">
+        <h1>Hi {{ $user['full_name'] }}</h1>
 
-    <p>Request from {!! nl2br(e($message)) !!} want to manage you! <a href='{{ url("/api/v1/driver-manager/request") }}/{{ rawurlencode($user->id) }}/{{ auth()->id() }}'  class='btn btn-primary'>  click here to accept  </a></p> | <a href='{{ url("/api/v1/driver-manager/request") }}/{{ rawurlencode($user->id) }}/{{ auth()->id() }}'  class='btn btn-danger'>  click here to reject  </a></p>
+        <p>Request from {!! nl2br(e($message)) !!} want to manage you! </p>
+        <div class="d-grid gap-2">
+            <a href='{{ url("/api/v1/driver-manager/request") }}/{{ rawurlencode($user->id) }}/{{ auth()->id() }}'  class='btn btn-primary'>Click here to accept</a>
+            <a href='{{ url("/api/v1/driver-manager/request") }}/{{ rawurlencode($user->id) }}/{{ auth()->id() }}'  class='btn btn-danger'>Click here to reject</a>
+        </div>
 
+        @if(isset($user['location']))
+            <p><em>Location: {{  $user['location']['ip'] }} | {{ $user['location']['countryName'] }} | {{ $user['location']['regionName'] }}</em></p>
+        @endif
 
-    @if(isset($user['location']))
-        <p><em>Location: {{  $user['location']['ip'] }} | {{ $user['location']['countryName'] }} | {{ $user['location']['regionName'] }}</em></p>
-    @endif
+        <br><br>
 
-    <br><br>
+        <p><em style="color: red">Remember:</em> <em>Keep your card and Pin information secure. Do not respond to emails requesting for your card/PIN details. If you think an email is suspicious, don't click on any links. Instead, forward it to our customer support.</em></p>
 
-    <p><em style="color: red">Remember:</em> <em>Keep your card and Pin information secure. Do not respond to emails requesting for your card/PIN details. If you think an email is suspicious, don't click on any links. Instead, forward it to our customer support.</em></p>
+        <br>
 
-    <br>
-
-    <p>Thanks,<br><br>{{ config('app.name') }}</p>
+        <p>Thanks,<br><br>{{ config('app.name') }}</p>
+    </div>
 </body>
 </html>
