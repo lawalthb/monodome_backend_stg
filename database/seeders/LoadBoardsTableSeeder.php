8096c4fe17e14718513f90010bec9d2c6ff66e49
DON EJEH <donejeh@gmail.com>
2024-03-01 17:53:23 +0100

    artisan

--- database/seeders/LoadBoardsTableSeeder.php
+++ database/seeders/LoadBoardsTableSeeder.php
@@ -14,12 +14,12 @@ class LoadBoardsTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('load_boards')->delete();
-        
+
         \DB::table('load_boards')->insert(array (
-            0 => 
+            0 =>
             array (
                 'id' => '1',
                 'uuid' => '683db90f-0782-4ee9-9595-c824663ccbe6',
@@ -37,7 +37,7 @@ class LoadBoardsTableSeeder extends Seeder
                 'created_at' => '2024-02-17 10:35:54',
                 'updated_at' => '2024-02-17 10:35:54',
             ),
-            1 => 
+            1 =>
             array (
                 'id' => '2',
                 'uuid' => '683db90f-0782-4ee9-9595-c824663ccbe6',
@@ -55,10 +55,10 @@ class LoadBoardsTableSeeder extends Seeder
                 'created_at' => '2024-02-17 10:40:49',
                 'updated_at' => '2024-02-17 10:40:49',
             ),
-            2 => 
+            2 =>
             array (
                 'id' => '3',
-                'uuid' => '683db90f-0782-4ee9-9595-c824663ccbe6',
+                'uuid' => '683db90f-0982-4ee9-9595-c824663ccbe6',
                 'user_id' => '3',
                 'acceptable_id' => NULL,
                 'acceptable_type' => NULL,
@@ -66,14 +66,14 @@ class LoadBoardsTableSeeder extends Seeder
                 'load_type_name' => 'package',
                 'status' => 'pending',
                 'status_comment' => NULL,
-                'order_no' => 'MO-86309211',
+                'order_no' => 'MO-45504007',
                 'load_date' => '2024-02-17 11:40:49',
                 'loadable_id' => '2',
-                'loadable_type' => 'App\\Models\\LoadPackage',
+                'loadable_type' => 'App\\Models\\Bulk',
                 'created_at' => '2024-02-17 10:40:49',
                 'updated_at' => '2024-02-17 10:40:49',
             ),
-            3 => 
+            3 =>
             array (
                 'id' => '4',
                 'uuid' => '683db90f-0782-4ee9-9595-c824663ccbe6',
@@ -81,17 +81,17 @@ class LoadBoardsTableSeeder extends Seeder
                 'acceptable_id' => NULL,
                 'acceptable_type' => NULL,
                 'load_type_id' => '1',
-                'load_type_name' => 'package',
+                'load_type_name' => 'bulk',
                 'status' => 'pending',
                 'status_comment' => NULL,
                 'order_no' => 'MO-72743822',
                 'load_date' => '2024-02-17 12:19:08',
                 'loadable_id' => '3',
-                'loadable_type' => 'App\\Models\\LoadPackage',
+                'loadable_type' => 'App\\Models\\Bulk',
                 'created_at' => '2024-02-17 11:19:08',
                 'updated_at' => '2024-02-17 11:19:08',
             ),
-            4 => 
+            4 =>
             array (
                 'id' => '5',
                 'uuid' => '683db90f-0782-4ee9-9595-c824663ccbe6',
@@ -99,17 +99,17 @@ class LoadBoardsTableSeeder extends Seeder
                 'acceptable_id' => NULL,
                 'acceptable_type' => NULL,
                 'load_type_id' => '1',
-                'load_type_name' => 'package',
+                'load_type_name' => 'bulk',
                 'status' => 'pending',
                 'status_comment' => NULL,
                 'order_no' => 'MO-45504237',
                 'load_date' => '2024-02-17 12:28:05',
                 'loadable_id' => '4',
-                'loadable_type' => 'App\\Models\\LoadPackage',
+                'loadable_type' => 'App\\Models\\Bulk',
                 'created_at' => '2024-02-17 11:28:05',
                 'updated_at' => '2024-02-17 11:28:05',
             ),
-            5 => 
+            5 =>
             array (
                 'id' => '6',
                 'uuid' => '683db90f-0782-4ee9-9595-c824663ccbe6',
@@ -117,18 +117,18 @@ class LoadBoardsTableSeeder extends Seeder
                 'acceptable_id' => NULL,
                 'acceptable_type' => NULL,
                 'load_type_id' => '1',
-                'load_type_name' => 'package',
+                'load_type_name' => 'bulk',
                 'status' => 'pending',
                 'status_comment' => NULL,
-                'order_no' => 'MO-45504237',
+                'order_no' => 'MO-45504200',
                 'load_date' => '2024-02-17 12:28:05',
                 'loadable_id' => '4',
-                'loadable_type' => 'App\\Models\\LoadPackage',
+                'loadable_type' => 'App\\Models\\Bulk',
                 'created_at' => '2024-02-17 11:28:05',
                 'updated_at' => '2024-02-17 11:28:05',
             ),
         ));
-        
-        
+
+
     }
 }
