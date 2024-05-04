1d870b51e3161c2779bf86c12be477b014ed7288
DON EJEH1~ <donejeh@gmail.com>
2024-05-04 23:07:45 +0100

    tracking_id

--- app/Models/Tracking.php
+++ app/Models/Tracking.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Tracking extends Model
 {
     use HasFactory;
+
+    public $guarded = [];
+
+
+
+    protected static function boot()
+    {
+        parent::boot();
+        self::creating(function($model){
+            $model->tracking_id =  Str::uuid()->toString();
+           // $model->user_id =  auth()->id();
+        });
+    }
 }
